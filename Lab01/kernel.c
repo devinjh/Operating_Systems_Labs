cbff411fd9581252e93a4bb69e9e36c83507521f
Tristan Hess <tlh173@zips.uakron.edu>
2019-09-12 16:41:15 -0400

    Add files via upload
    Add completed lab files (Tristan)

--- Lab01/kernel.c
+++ Lab01/kernel.c
@@ -19,7 +19,7 @@
 /*                                                                        */
 /*                                                                        */
 /*                                                                        */
-/* Signed:_____________________________________ Date:_____________        */
+/* Signed:Tristan Hess_________________________ Date:9/12/2019____        */
 /*                                                                        */
 /*                                                                        */
 /* 3460:4/526 BlackDOS2020 kernel, Version 1.03, Fall 2019.               */
@@ -31,45 +31,38 @@ void main()
 {
    makeInterrupt21();
    printLogo();
-   interrupt(33,0,"Hello world from Devin H, Tristan H, and Andrew R.\r\n\0",1,0);
+   interrupt(33,0,"Hello world from Andrew, Tristan, and Devin.\r\n\0",1,0);
    while(1);
 }
 
 void printString(char* c, int d)
 {
-	int i = 0;
-	if (d == 1)
-	{
-        	while ((*c) != '\0')
-        	{
-                	interrupt(23, *c, 0, 0, 0);
-			*c++;
-        	}
-	}
-	else
-	{
-        	while ((*c) != '\0')
-        	{
-                	char al = *c++;
-                	char ah = 14;
-                	int ax = ah * 256 + al;
-                	interrupt(16, ax, 0, 0, 0);
-        	}
-	}
+   while((*c) != '\0') {
+      char al = *c++;
+      char ah = 14;
+      int ax = ah * 256 + al;
 
-	return;
+      if(d == 1) {
+         interrupt(23, al, 0, 0, 0);
+      }
+      else {
+         interrupt(16, ax, 0, 0, 0);
+      }
+   };
+
+   return; 
 }
 
 void printLogo()
 {
-   printString("       ___   `._   ____  _            _    _____   ____   _____ \r\n\0",0);
-   printString("      /   \\__/__> |  _ \\| |          | |  |  __ \\ / __ \\ / ____|\r\n\0",0);
-   printString("     /_  \\  _/    | |_) | | __ _  ___| | _| |  | | |  | | (___ \r\n\0",0);
-   printString("    // \\ /./      |  _ <| |/ _` |/ __| |/ / |  | | |  | |\\___ \\ \r\n\0",0);
-   printString("   //   \\\\        | |_) | | (_| | (__|   <| |__| | |__| |____) |\r\n\0",0);
-   printString("._/'     `\\.      |____/|_|\\__,_|\\___|_|\\_\\_____/ \\____/|_____/\r\n\0",0);
-   printString(" BlackDOS2020 v. 1.03, c. 2019. Based on a project by M. Black. \r\n\0",0);
-   printString(" Author(s): Devin H, Tristan H, Andrew R.\r\n\r\n\0",0);
+   interrupt(33,0,"       ___   `._   ____  _            _    _____   ____   _____ \r\n\0",0,0);
+   interrupt(33,0,"      /   \\__/__> |  _ \\| |          | |  |  __ \\ / __ \\ / ____|\r\n\0",0,0);
+   interrupt(33,0,"     /_  \\  _/    | |_) | | __ _  ___| | _| |  | | |  | | (___ \r\n\0",0,0);
+   interrupt(33,0,"    // \\ /./      |  _ <| |/ _` |/ __| |/ / |  | | |  | |\\___ \\ \r\n\0",0,0);
+   interrupt(33,0,"   //   \\\\        | |_) | | (_| | (__|   <| |__| | |__| |____) |\r\n\0",0,0);
+   interrupt(33,0,"._/'     `\\.      |____/|_|\\__,_|\\___|_|\\_\\_____/ \\____/|_____/\r\n\0",0,0);
+   interrupt(33,0," BlackDOS2020 v. 1.03, c. 2019. Based on a project by M. Black. \r\n\0",0,0);
+   interrupt(33,0," Author(s): Andrew Robinson, Tristan Hess, Devin Hopkins.\r\n\r\n\0",0,0);
 }
 
 /* MAKE FUTURE UPDATES HERE */
@@ -82,56 +75,16 @@ void printLogo()
 
 void handleInterrupt21(int ax, int bx, int cx, int dx)
 {
+/*   return; */
    switch(ax) {
       case 0:
-	      printString(bx, cx);
-	      break;
-      /*case 1:
-	      //
-	      break;
-      case 2:
-	      //
-	      break;
-      case 3:
-	      //
-	      break;
-      case 4:
-	      //
-	      break;
-      case 5:
-	      //
-	      break;
-      case 6:
-	      //
-	      break;
-      case 7:
-	      //
-	      break;
-      case 8:
-	      //
-	      break;
-      case 9:
-	      //
-	      break;
-      case 10:
-	      //
-	      break;
-      case 11:
-	      //
-	      break;
-      case 12:
-	      //
-	      break;
-      case 13:
-	      //
-	      break;
-      case 14:
-	      //
-	      break;
-      case 15:
-	      //
-	      break;*/
+        printString(bx,cx);
+	break; 
+/*      case 1: case 2: case 3: case 4: case 5: */
+/*      case 6: case 7: case 8: case 9: case 10: */
+/*      case 11: case 12: case 13: case 14: case 15: */ 
       default:
-	      printString("General BlackDOS error.\r\n\0");
-   }
+	printString("General BlackDOS error.\r\n\0");
+	break; 
+   }  
 }
