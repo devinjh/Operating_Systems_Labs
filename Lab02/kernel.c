4f7e65629d602bbfa1b90a622a9abc02e4d7c07b
Andrew Robinson <alr104@zips.uakron.edu>
2019-09-17 17:30:08 -0400

    Finish lab2

--- Lab02/kernel.c
+++ Lab02/kernel.c
@@ -29,17 +29,47 @@ void printLogo();
 
 void main()
 {
-  int i;
-  char buffer[80];
-  for(i = 0; i < 80; ++i)buffer[i] = 0;
-
-  makeInterrupt21();
-  printLogo();
-  interrupt(33, 0, "Hello world from Andrew :)\r\n\0", 1, 0);
-  interrupt(33, 14, i, 0, 0);
-  interrupt(33, 13, i, 0, 0);
-  while(1);
+  char food[25], adjective[25], color[25], animal[25];
+   int temp;
+   makeInterrupt21();
+   printLogo();
+   interrupt(33,0,"\r\nWelcome to the Mad Libs kernel.\r\n\0",0,0);
+   interrupt(33,0,"Enter a food: \0",0,0);
+   interrupt(33,1,food,0,0);
+   temp = 0;
+   while ((temp < 100) || (temp > 120)) {
+      interrupt(33,0,"Enter a number between 100 and 120: \0",0,0);
+      interrupt(33,14,&temp,0,0);
+   }
+   interrupt(33,0,"Enter an adjective: \0",0,0);
+   interrupt(33,1,adjective,0,0);
+   interrupt(33,0,"Enter a color: \0",0,0);
+   interrupt(33,1,color,0,0);
+   interrupt(33,0,"Enter an animal: \0",0,0);
+   interrupt(33,1,animal,0,0);
+   interrupt(33,0,"Your note is on the printer, go get it.\r\n\0",0,0);
+   interrupt(33,0,"Dear Professor O\'Neil,\r\n\0",1,0);
+   interrupt(33,0,"\r\nI am so sorry that I am unable to turn in my program at this time.\r\n\0",1,0);
+   interrupt(33,0,"First, I ate a rotten \0",1,0);
+   interrupt(33,0,food,1,0);
+   interrupt(33,0,", which made me turn \0",1,0);
+   interrupt(33,0,color,1,0);
+   interrupt(33,0," and extremely ill.\r\n\0",1,0);
+   interrupt(33,0,"I came down with a fever of \0",1,0);
+   interrupt(33,13,temp,1,0);
+   interrupt(33,0,". Next my \0",1,0);
+   interrupt(33,0,adjective,1,0);
+   interrupt(33,0," pet \0",1,0);
+   interrupt(33,0,animal,1,0);
+   interrupt(33,0," must have\r\nsmelled the remains of the \0",1,0);
+   interrupt(33,0,food,1,0);
+   interrupt(33,0," on my computer, because he ate it. I am\r\n\0",1,0);
+   interrupt(33,0,"currently rewriting the program and hope you will accept it late.\r\n\0",1,0);
+   interrupt(33,0,"\r\nSincerely,\r\n\0",1,0);
+   interrupt(33,0,"The boys\r\n\0",1,0);
+   while(1);
 }
+
 
 void printLogo()
 {
@@ -74,9 +104,16 @@ void writeInt(int n, int cx)
 {
   char numToPrint[6];/* All are 5 digit numbers*/
   int i;
+  int x = n;
+  int end = 0;
 
-  numToPrint[5] = '\0';
-  for(i = 4; i >= 0; --i) {
+  while(x != 0) {
+    x = div(x, 10);
+    ++end;
+  }
+
+  numToPrint[end] = '\0';
+  for(i = end - 1; i >= 0; --i) {
     numToPrint[i] = mod(n, 10) + 48;
     n = div(n, 10);
   }
