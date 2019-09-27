0ae0c1a16f7b6523bf8d9583e52a2b8c0aa1d2b4
Andrew Robinson <alr104@zips.uakron.edu>
2019-09-27 15:13:55 -0400

    Create Shell.c basm.o and update shell script to compile, link, and put on disk at sector 30

--- compileOS.sh
+++ compileOS.sh
@@ -4,4 +4,6 @@ bcc -ansi -c -o kernel.o kernel.c
 ld86 -o kernel -d kernel.o kasm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=259
 dd if=config of=floppya.img bs=512 count=1 seek=258 conv=notrunc
-dd if=fib of=floppya.img bs=512 count=1 seek=30 conv=notrunc
+bcc -ansi -c -o Shell.o Shell.c
+ld86 -o Shell -d Shell.o basm.o
+dd if=Shell of=floppya.img bs=512 count=1 seek=30 conv=notrunc
